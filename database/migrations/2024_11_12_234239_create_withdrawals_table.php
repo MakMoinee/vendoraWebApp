23441db422bfaac674b5cbad307ac0b3e632dd59
Mak Moinee <titus.silver.ph@gmail.com>
2024-11-13 07:44:43 +0800

    update code

---
+++ database/migrations/2024_11_12_234239_create_withdrawals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('withdrawals', function (Blueprint $table) {
+            $table->id('withdrawID')->autoIncrement();
+            $table->integer('userID')->nullable(false);
+            $table->integer('machineID')->nullable(false);
+            $table->string('denomination')->nullable(false);
+            $table->decimal('total', 10, 2)->nullable(false);
+            $table->decimal('remaining', 10, 2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('withdrawals');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\UserHomeController;
 use App\Http\Controllers\UserMachineController;
 use App\Http\Controllers\UserReportsController;
+use App\Http\Controllers\UserSavingsController;
+use App\Http\Controllers\UserWithdrawals;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +23,6 @@ Route::resource('/', WelcomeController::class);
 Route::resource('/user_home', UserHomeController::class);
 Route::resource('/user_machine', UserMachineController::class);
 Route::resource('/reports', UserReportsController::class);
+Route::resource('/savings', UserSavingsController::class);
+Route::resource('/withdraw', UserWithdrawals::class);
 Route::get('/logout', [WelcomeController::class, 'logout']);
