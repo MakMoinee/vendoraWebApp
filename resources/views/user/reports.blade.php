c2c7a607e74517eae2d34458e9ff1a96b5f97cc3
Mak Moinee <titus.silver.ph@gmail.com>
2024-11-22 14:03:31 +0800

    update code

--- resources/views/user/reports.blade.php
+++ resources/views/user/reports.blade.php
@@ -186,7 +186,8 @@
                         <div class="card-header d-flex">
                             <h5>{{ $prefix }} Sales Graph</h5>
                             <br>
-                            <button style="margin-left: 20px;" class="btn btn-success" onclick="generatePDF()">
+                            <button style="margin-left: 20px;" class="btn btn-success" data-bs-toggle="modal"
+                                data-bs-target="#reportModal">
                                 Export
                             </button>
                         </div>
@@ -327,6 +328,43 @@
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
+
+    <div class="modal fade" id="reportModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="reportModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="reportModalLabel">Report</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table border mb-0">
+                                            <thead class="table-light fw-semibold">
+                                                <tr class="align-middle">
+                                                    <th class="text-center">Month & Year / Week</th>
+                                                    <th>Total</th>
+                                                    <th class="text-center"></th>
+                                                </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary">Export</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="modal fade" id="addMachineModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="addMachineModalLabel" aria-hidden="true">
